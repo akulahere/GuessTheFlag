96916abee1d49b5e8bca9821add90f20fc878de2
akula <akulinind@gmail.com>
2022-08-01 12:22:17 +0300

    feat: update design

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,24 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var showingAlert = false
-
+  @State private var showingScore = false
+  @State private var scoreTitle = ""
+  @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+  @State var correctAnswer = Int.random(in: 0...2)
+  
   var body: some View {
-      Button("Show Alert") {
-        showingAlert = true
+    ZStack {
+      RadialGradient(stops: [
+        .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+        .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+      ], center: .top, startRadius: 200, endRadius: 400)
+      .ignoresSafeArea()
+      
+      VStack {
+        Text("Guess the flag")
+          .font(.largeTitle.bold())
+          .foregroundColor(.white)
+        
+        Text("Score: ???")
+          .foregroundColor(.white)
+          .font(.title.bold())
+        
+        VStack(spacing: 15) {
+          VStack {
+            Text("Tap the flag of")
+              .font(.subheadline.weight(.heavy))
+              .foregroundStyle(.secondary)
+            Text(countries[correctAnswer])
+              .font(.largeTitle.weight(.semibold))
+          }
+          ForEach(0..<3) { number in
+            Button {
+              flagTapped(number)
+            } label: {
+              Image(countries[number])
+                .renderingMode(.original)
+                .clipShape(Capsule())
+                .shadow(radius: 5)
+            }
+          }
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.vertical, 20)
+        .background(.regularMaterial)
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+        
+        Spacer()
+        Spacer()
+        
+        Text("Score: ???")
+          .foregroundColor(.white)
+          .font(.title.bold())
+        
+        Spacer()
       }
-      .alert("Important message", isPresented: $showingAlert) {
-        Button("OK", role: .destructive) { }
-      } message: {
-        Text("Please read this.")
-      }
-
+      .padding()
+    }.alert(scoreTitle, isPresented: $showingScore) {
+      Button("Continue", action: askQuestion)
+    } message: {
+      Text("Your score is ???")
     }
+  }
+  
+  func flagTapped(_ number: Int) {
+    if number == correctAnswer {
+      scoreTitle = "Correct"
+    } else {
+      scoreTitle = "Wrong"
+    }
+    
+    showingScore = true
+  }
+  
+  func askQuestion() {
+    countries.shuffle()
+    correctAnswer = Int.random(in: 0...2)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
       ContentView()
-        .previewDevice("iPhone 11 Pro Max")
+        .previewDevice("iPhone 8")
     }
 }
