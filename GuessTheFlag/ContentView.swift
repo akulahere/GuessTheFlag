c8631500fda6d2d3008bb5ef984115c2b274ea11
akula <akulinind@gmail.com>
2022-08-02 12:36:49 +0300

    feat: add game cycle

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
   @State private var showingScore = false
+  @State private var showingResults = false
+
   @State private var scoreTitle = ""
   @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
   @State var correctAnswer = Int.random(in: 0...2)
+  @State var score = 0
+  @State private var questionCounter = 1
+
   
   var body: some View {
     ZStack {
@@ -25,10 +30,7 @@ struct ContentView: View {
         Text("Guess the flag")
           .font(.largeTitle.bold())
           .foregroundColor(.white)
-        
-        Text("Score: ???")
-          .foregroundColor(.white)
-          .font(.title.bold())
+      
         
         VStack(spacing: 15) {
           VStack {
@@ -57,7 +59,7 @@ struct ContentView: View {
         Spacer()
         Spacer()
         
-        Text("Score: ???")
+        Text("Score: \(score)")
           .foregroundColor(.white)
           .font(.title.bold())
         
@@ -67,24 +69,48 @@ struct ContentView: View {
     }.alert(scoreTitle, isPresented: $showingScore) {
       Button("Continue", action: askQuestion)
     } message: {
-      Text("Your score is ???")
+      Text("Your score is \(score)")
     }
+    .alert("Game over!", isPresented: $showingResults) {
+      Button("Start Again", action: newGame)
+    } message: {
+      Text("Your final score was \(score).")
+    }
+    
   }
   
   func flagTapped(_ number: Int) {
     if number == correctAnswer {
       scoreTitle = "Correct"
+      score += 1
     } else {
       scoreTitle = "Wrong"
+      
+      if score > 0 {
+        score -= 1
+      }
     }
     
-    showingScore = true
+    if questionCounter == 8 {
+      showingResults = true
+    } else {
+      showingScore = true
+    }
+    
   }
   
   func askQuestion() {
     countries.shuffle()
     correctAnswer = Int.random(in: 0...2)
+    questionCounter += 1
   }
+  
+  func newGame() {
+    questionCounter = 0
+    score = 0
+    askQuestion()
+  }
+  
 }
 
 struct ContentView_Previews: PreviewProvider {
